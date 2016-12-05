59572359a1d427a120a3f6fdebfa155b6b59582e
Mark Edelman <markedelman1987@gmail.com>
2016-12-05 14:31:42 -0500

    first button rendered question

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 
 // Create your initial state object
 var state = {
-    
+
     // Questions
- 	questions: [      
-{question: 'How does protoypal inhertitance work in JavaScript?',
-answerOptions: ['Donald Trump','Hillary Clinton','Bernie Sanders'],
-rightAnswer: 0},
- 
- {question: 'What is a high order function?',
-answerOptions: ['Wim Hof','Nikola Tesla','Chuck Norris'],
-rightAnswer: 1},
- 
- {question: 'What is the difference between == and === ?',
-answerOptions: ['Africa','== checks value and allows coercion, === checks if the value and data type match without coercion'
- ,'North America'],
-rightAnswer: '== checks value and allows coercion of the left element to the right elements datatype, === checks if the value and data type match without coercion'
+    questions: [      
+    {question: 'How does protoypal inhertitance work in JavaScript?',
+    answerOptions: ['Donald Trump','Hillary Clinton','Bernie Sanders'],
+    rightAnswer: 0},
+
+    {question: 'What is a high order function?',
+    answerOptions: ['Wim Hof','Nikola Tesla','Chuck Norris'],
+    rightAnswer: 1},
+
+    {question: 'What is the difference between == and === ?',
+    answerOptions: ['Africa','== checks value and allows coercion, === checks if the value and data type match without coercion'
+    ,'North America'],
+    rightAnswer: '== checks value and allows coercion of the left element to the right elements datatype, === checks if the value and data type match without coercion'
 },
- 
- {question: 'What value does a variable get if you do not assign it a value?',
+
+{question: 'What value does a variable get if you do not assign it a value?',
 answerOptions: ['null','undefined','0'],
 rightAnswer: 1},
- 
- {question: 'What does bang bang mean (!!) ?',
+
+{question: 'What does bang bang mean (!!) ?',
 answerOptions: ['tests if it is true','tests if it is false','superman'],
 rightAnswer: 0}
- 	],
+],
 
- 	rightAnswerCounter: 0,
- 	currentQuestion: 0,
+rightAnswerCounter: 0,
+currentQuestion: 0,
 
- 	route: 'Welcome'
+route: 'Welcome'
     // Answers
     // Your choice of answer	
     // Message(s) to let them know they have the correct answer	
@@ -43,11 +43,21 @@ rightAnswer: 0}
 function renderNextQuestion(state){
 
 }  
+function renderStart(state){
+	$('.js-welcome').addClass('hidden');
+	$('.js-question').removeClass('hidden');
+	$('.js-answer-count').removeClass('hidden');
+
+}
 // Event handlers
 // When start button is submitted
-$('.start').submit(function(event) {
+$('.js-start').click(function(event) {
+	event.preventDefault();
+	console.log('help');
 	state.route = 'Started';
-
+	state.currentQuestion = 1;
+	state.rightAnswerCounter = 0;
+	renderStart();
 });
 
 // Current answer is submitted
