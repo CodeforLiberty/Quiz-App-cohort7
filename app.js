432eb0ee8971aa7919006c7eb5ab4e5569f86ec4
Mark Edelman <markedelman1987@gmail.com>
2016-12-05 12:45:28 -0500

    Made state object

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 
 // Create your initial state object
 var state = {
+    
     // Questions
+ 	questions: [      
+{question: 'How does protoypal inhertitance work in JavaScript?',
+answerOptions: ['Donald Trump','Hillary Clinton','Bernie Sanders'],
+rightAnswer: 0},
+ 
+ {question: 'What is a HOF',
+answerOptions: ['Wim Hof','Nikola Tesla','Chuck Norris'],
+rightAnswer: 1},
+ 
+ {question: 'What is the difference between == and === ?',
+answerOptions: ['Africa','== checks value and allows coercion, === checks if the value and data type match without coercion'
+ ,'North America'],
+rightAnswer: '== checks value and allows coercion, === checks if the value and data type match without coercion'
+}
+ 
+ {question: 'What value does a variable get if you do not assign it a value?',
+answerOptions: ['null','undefined','0'],
+rightAnswer: 1}
+ 
+ {question: 'What does bang bang mean (!!) ?',
+answerOptions: ['tests if it is true','tests if it is false','superman'],
+rightAnswer: 0}
+ 	],
+
+ 	rightAnswerCounter: 0,
+ 	currentQuestion: 0,
+
+ 	route: 'Welcome'
     // Answers
     // Your choice of answer	
     // Message(s) to let them know they have the correct answer	
@@ -11,10 +40,14 @@ var state = {
 
 // State manipulation functions
 // Render functions
+function renderNextQuestion(state){
 
+}  
 // Event handlers
 // When start button is submitted
 $('.start').submit(function(event) {
+	state.route = 'Started';
+
 });
 
 // Current answer is submitted
