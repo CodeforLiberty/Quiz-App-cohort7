53dae28082768b43f8fc8f5f5f54f76bf882e062
CodeforLiberty <sswell@live.com>
2016-12-05 11:37:31 -0500

    BoilerPlate

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// Create your initial state object
+var state = {
+    // Questions
+    // Answers
+    // Your choice of answer	
+    // Message(s) to let them know they have the correct answer	
+    // Message(s) when they don't have the correct answer
+    // Other things like score? Anything else?
+};
+
+// State manipulation functions
+// Render functions
+
+// Event handlers
+// When start button is submitted
+$('.start').submit(function(event) {
+});
+
+// Current answer is submitted
+$('.answer').submit(function(event) {
+});
+
+// Next question
+$('.next').click(function(event) {
+});
+
+// Restart button is clicked
+$('.restart').click(function(event) {
+});
