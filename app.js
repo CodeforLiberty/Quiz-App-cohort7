67397e0dbbea8bf5ef9c11e7510ae86268588575
CodeforLiberty <sswell@live.com>
2016-12-07 09:58:00 -0500

    Formatting changes

--- app.js
+++ app.js
@@ -3,34 +3,31 @@
 
 var state = {
     // Questions
-    questions: [      
-    {question: 'When you create a button inside of a form without a submit button what will it do?',
-    answerOptions: ['It will attach its submit function to it automatically ','??','It will run the buttons code from your Js as expected'],
-    rightAnswer: 0},
-
-    {question: 'What is a high order function?',
-    answerOptions: ['Wim Hof','Nikola Tesla','Chuck Norris'],
-    rightAnswer: 1},
-
-    {question: 'What is the difference between == and === ?',
-    answerOptions: ['Africa','== checks value and allows coercion, === checks if the value and data type match without coercion'
-    ,'North America'],
-    rightAnswer:1
-	},
-
-	{question: 'What value does a variable get if you do not assign it a value?',
-	answerOptions: ['null','undefined','0'],
-	rightAnswer: 1},
-
-	{question: 'What does bang bang mean (!!) ?',
-	answerOptions: ['tests if it is true','tests if it is false','superman'],
-	rightAnswer: 0}
-	],
+    questions: [{
+    	question: 'When you create a button inside of a form without a submit button what will it do?',
+    	answerOptions: ['It will attach its submit function to it automatically ','??','It will run the buttons code from your Js as expected'],
+    	rightAnswer: 0
+    }, {
+    	question: 'What is a high order function?',
+    	answerOptions: ['Wim Hof','Nikola Tesla','Chuck Norris'],
+    	rightAnswer: 1
+    }, {
+    	question: 'What is the difference <br /> between == and === ?',
+    	answerOptions: ['Africa','== checks value and allows coercion, === checks if the value and data type match without coercion'
+    	,'North America'],
+    	rightAnswer:1
+	}, {
+		question: 'What value does a variable get if you do not assign it a value?',
+		answerOptions: ['null','undefined','0'],
+		rightAnswer: 1
+	}, {
+		question: 'What does bang bang mean (!!) ?',
+		answerOptions: ['tests if it is true','tests if it is false','superman'],
+		rightAnswer: 0
+	}],
 
 	right: 0,
 	currentQuestion: 0,
-
-	route: 'Welcome'
     
 };
 
@@ -38,26 +35,28 @@ var state = {
 // Render functions
 function checkAnswer (state, selectedAnswer) {
  
-var correctAnswer = state.questions[state.currentQuestion].rightAnswer;
+	var correctAnswer = state.questions[state.currentQuestion].rightAnswer;
 
 
-if (selectedAnswer == correctAnswer) {
+	if (+selectedAnswer === correctAnswer) {
 
-	alert("YOU ARE RIGHT");
-	state.right++;
-	state.currentQuestion++;
-	renderStart(state);
-}
-if (selectedAnswer === undefined) {
+		alert("YOU ARE RIGHT");
+		state.right++;
+		state.currentQuestion++;
+		renderStart(state);
+	}
+	
+	if (selectedAnswer === undefined) {
 
-	alert("Select an answer");
-}
-if ( selectedAnswer !== undefined && selectedAnswer != correctAnswer) {
+		alert("Select an answer");
+	}
+	
+	if ( !!selectedAnswer && + selectedAnswer !== correctAnswer) {
 
-	alert("YOU ARE WRONG");
-	state.currentQuestion++;
-	renderStart(state);
-}
+		alert("YOU ARE WRONG");
+		state.currentQuestion++;
+		renderStart(state);
+	}
 }
 
 function renderEnd(state) {
@@ -65,7 +64,7 @@ function renderEnd(state) {
 	${state.currentQuestion}</p><br><button class='retry'>Retry</button` );
 	$('.js-answer-count').removeClass().addClass('hidden');
 
-	}
+}
 
 function renderStart(state){
 	if( state.currentQuestion == 5) {
@@ -74,21 +73,33 @@ function renderStart(state){
 	}
 	var questionNumberUpdate = state.currentQuestion +1; 
 	var possibleAnswers = state.questions[state.currentQuestion].answerOptions;
-	 $('.js-welcome').addClass('hidden'); //Hide welcome message
-	 $('.js-question').removeClass('hidden');//Show the question
-	 $('.js-answer-count').removeClass('hidden');//Display count
-	 $('#js-correct-answers').html(state.right);
-	 $('#js-total-answers').html(state.questions.length);
-	 $('#js-question-number').html(questionNumberUpdate);	
 	
-	 $('.js-question').html(`<p>Question Number ${questionNumberUpdate}</br>
-	<span id="js-question-number"></span>${state.questions[state.currentQuestion].question}  
-	</p><form class="js-question-form"></form><button id = "next"> NEXT </button><button class="restart">Restart</button>`);
+	$('.js-welcome').addClass('hidden'); //Hide welcome message
+	$('.js-question').removeClass('hidden');//Show the question
+	$('.js-answer-count').removeClass('hidden');//Display count
+	$('#js-correct-answers').html(state.right);
+	$('#js-total-answers').html(state.questions.length);
+	$('#js-question-number').html(questionNumberUpdate);	
 
-	var renderOptions = `<input type="radio" name="answer" value="0" required><label> ${possibleAnswers[0]} </label> 
-		<br><input type="radio" name="answer" value="1"> <label>${possibleAnswers[1]}</label><br><input
-		 type="radio" name="answer" value="2"><label> ${possibleAnswers[2]}</label><br><input
-		  type="radio" name="answer" value="3"><label> ${possibleAnswers[3]}</label>`;
+	$('.js-question').html(`
+		<p>
+			Question Number <span id="js-question-number">${questionNumberUpdate}</span>
+			
+		</p> 
+		<p> ${state.questions[state.currentQuestion].question} </p>
+		
+		<form class="js-question-form"></form>
+		<button id = "next"> NEXT </button>
+		<button class="restart">Restart</button>
+	`
+	);
+
+	var renderOptions = `
+		<input type="radio" name="answer" value="0" required><label> ${possibleAnswers[0]} </label> <br>
+		<input type="radio" name="answer" value="1"> <label>${possibleAnswers[1]}</label><br>
+		<input type="radio" name="answer" value="2"><label> ${possibleAnswers[2]}</label><br>
+		<input type="radio" name="answer" value="3"><label> ${possibleAnswers[3]}</label>
+	`;
 	
  $('.js-question-form').html(renderOptions);
 
